419142c4bfdda06535a2f9315e2072aff37aec84
midnitesun002 <midnitesun002@users.noreply.github.com>
2025-05-29 09:06:49 -0400

    Update .docker-prompt.sh

--- .docker-prompt.sh
+++ .docker-prompt.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
 
+__docker_status_ps1()
+{
+    CONTEXT=$(docker ps | wc -l)
+
+    if [[ $CONTEXT -gt 1 ]]; then
+        echo -e "(docker: up)"
+    else
+        echo -e "(docker: idle)"
+    fi
+}
