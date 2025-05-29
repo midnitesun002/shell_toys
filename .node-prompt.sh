2a7a85f82b445ef7d7f3c746f3a5eacd4718dab4
midnitesun002 <midnitesun002@users.noreply.github.com>
2025-05-29 11:33:48 -0400

    Update .node-prompt.sh

--- .node-prompt.sh
+++ .node-prompt.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
 
+NODECOLOR="\e[38;5;16;48;5;237m"
+ENDCOLOR="\e[0m"
+__node_status()
+{
+    CONTEXT=$(node --version 2> /dev/null)
+
+    echo -e "${NODECOLOR}(node: ${CONTEXT})${ENDCOLOR}"
+}
