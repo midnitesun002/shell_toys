991506f255b7157cf207482e0365c8a0d5c83a12
midnitesun002 <midnitesun002@users.noreply.github.com>
2025-05-29 11:37:52 -0400

    Update .python-prompt.sh

--- .python-prompt.sh
+++ .python-prompt.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
 
+# Light Red - 91
+PYTHONCOLOR="\e[38;5;228;40m"
+ENDCOLOR="\e[0m"
+__python_status()
+{
+    CONTEXT=$(python --version 2> /dev/null | sed "s/Python //")
+
+    echo -e "${PYTHONCOLOR}(py ${CONTEXT})${ENDCOLOR}"
+}
