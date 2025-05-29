0ca925910ead14f1f2f133e30a74ebff9fff6cd3
midnitesun002 <midnitesun002@users.noreply.github.com>
2025-05-29 09:13:45 -0400

    Update .kube-prompt.sh

--- .kube-prompt.sh
+++ .kube-prompt.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
 
+# Cyan - 36
+KUBECOLOR="\e[36m"
+ENDCOLOR="\e[0m"
+
+__kube_status()
+{
+    CONTEXT=$(cat ~/.kubeconfig | grep "current-context:" | sed "s/current-context: //" | sed "s/\"//g")
+
+    echo -e "${KUBECOLOR}(k8s: ${CONTEXT})${ENDCOLOR}"
+}
