240c38ed965064c8650d3056cc2683eb8d7031c1
midnitesun002 <midnitesun002@users.noreply.github.com>
2025-05-29 09:11:18 -0400

    Update .git-prompt.sh

--- .git-prompt.sh
+++ .git-prompt.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
+# there is a __git_ps1 defined somewhere
 
+# Light Magenta - 95
+GITCOLOR="\e[95m"
+ENDCOLOR="\e[0m"
+
+__git_status()
+{
+    CONTEXT=$(git branch 2> /dev/null | grep "* \(.*\)" | sed "s/* //")
+
+    echo -e "${GITCOLOR}(git: ${CONTEXT})${ENDCOLOR}"
+}
